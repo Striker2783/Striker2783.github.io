f20040dca5683a32e0109c4b203281ced37839ca
Striker2783 <owenxingli@gmail.com>
2024-05-21 16:14:08 -0400

    Changed the HTML Structure
    nav-bar custom element no longer uses shadow dom.
    Transition for Project Links

--- components/nav-bar.js
+++ components/nav-bar.js
@@ -1,17 +1,14 @@
 class NavigationBar extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
+  }
 
-    this.shadowRoot.innerHTML = `
-    <nav class="top-bar">
+  connectedCallback() {
+    this.innerHTML = `
+    <nav>
         <a class="big-header header" href="">Striker2783's Portfolio</a>
     </nav>
     `;
-    const linkElem = document.createElement("link");
-    linkElem.setAttribute("rel", "stylesheet");
-    linkElem.setAttribute("href", "/components/nav-bar.css");
-    this.shadowRoot.appendChild(linkElem);
   }
 }
 
